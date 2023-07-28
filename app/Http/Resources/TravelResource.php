fd7238a2ef8c1a6453486e3a4b4d5de12965c121
Daniil <solovyov.d@sunbay.nl>
2023-07-28 21:28:49 +0300

    travel

---
+++ app/Http/Resources/TravelResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TravelResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'number_of_days' => $this->number_of_days,
+            'number_of_nights' => $this->number_of_nights,
+        ];
+    }
+}
