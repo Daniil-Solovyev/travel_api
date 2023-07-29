c3c1af5dbf2dc8329d698a23abd35fa31828d141
Daniil <solovyov.d@sunbay.nl>
2023-07-29 14:36:43 +0300

    tour sorts

--- tests/Feature/ToursListTest.php
+++ tests/Feature/ToursListTest.php
@@ -39,4 +39,34 @@ class ToursListTest extends TestCase
         $response->assertJsonCount(15, 'data');
         $response->assertJsonPath('meta.last_page', 2);
     }
+
+    public function test_tours_list_sorts_by_price_correctly()
+    {
+        $travel = Travel::factory()->create(['is_public' => true]);
+
+        $earlier_tour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 100,
+        ]);
+
+        $later_tour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 200,
+        ]);
+
+        $response = $this->get('api/v1/travels/' . $travel->slug . '/tours?sortBy=price&sortOrder=asc');
+
+        $response->assertStatus(200);
+        $response->assertJsonPath('data.0.id', $earlier_tour->id);
+        $response->assertJsonPath('data.1.id', $later_tour->id);
+    }
+
+    public function test_tour_list_returns_validation_errors()
+    {
+        $travel = Travel::factory()->create(['is_public' => true]);
+
+        $response = $this->get('api/v1/travels/' . $travel->slug . '/tours?priceFrom=asdasd');
+
+        $response->assertStatus(422);
+    }
 }
